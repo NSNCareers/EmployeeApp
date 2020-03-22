f4a086e699f93e4d32d2d500580539704e2bce55
Ajang Ngwesse <elvisdemoprojects@outlook.com>
2020-03-22 16:13:51 +0000

    First commit

---
+++ Dot.NetCoreWebApp/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dot.NetCoreWebApp.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options):base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
